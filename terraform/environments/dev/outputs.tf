0304d2aefed633289873e78029f85f173af74ba1
Saurabh <saurab.jv2@gmail.com>
2026-06-13 14:20:03 +0530

    implement github actions ci pipeline

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,3 +1,15 @@
+output "github_actions_role_arn" {
+  value = module.github_oidc.github_actions_role_arn
+}
+
 output "ecr_repository_url" {
   value = module.ecr.repository_url
+}
+
+output "aws_region" {
+  value = var.aws_region
+}
+
+output "repository_name" {
+  value = var.ecr_repo_name
 }
